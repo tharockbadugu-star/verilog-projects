fcaeac57fc0b5f711bd8a8b2631667b9f479b6ce
tharockbadugu-star <tharockbadugu@gmail.com>
2026-06-24 13:30:42 +0530

    Delete full-adder-rca/src directory

--- full-adder-rca/src/full-adder-rca/src/full_adder.v
+++
@@ -1,9 +0,0 @@
-module full_adder (input  a,input  b,input  cin,output sum output cout);
-  
-    wire sum_ab, carry_ab, carry_cin;
-
-    half_adder HA1 (.a(a),.b(b),.sum(sum_ab),.carry(carry_ab));
-    half_adder HA2 (.a(sum_ab),.b(cin),.sum(sum),.carry(carry_cin));
-
-    assign cout = carry_ab | carry_cin;
-endmodule
